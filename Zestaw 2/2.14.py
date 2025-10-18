eade9060f17b3344055db102e7dfa0711b30b5f7
Tori <150959748+callioste@users.noreply.github.com>
2025-10-18 23:17:50 +0200

    Update 2.14.py
    Updated according to feedback

--- Zestaw 2/2.14.py
+++ Zestaw 2/2.14.py
@@ -1,7 +1,6 @@
 line = "ala\nma\nkota i psa"
 l = line.split()
-key1 = lambda k: len(k)
-longest_word = max(l, key= key1)
+longest_word = max(l, key = len(k))
 
 print(longest_word)
 print(len(longest_word))
